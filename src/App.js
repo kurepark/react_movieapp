9b7a672cd92fd8919be9b2c5986ed31a33ce4a9c
eunhyePark <eunhye0406@gmail.com>
2022-05-18 16:01:39 +0900

    coins

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [selCoins, setSelCoins] = useState("0");
+  const [result, setResult] = useState("0");
+
+
+  const onChange = (event) => {
+    setSelCoins(event.target.value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const priceUSD = event.target[0].value;
+    calculate(priceUSD);
+  }
+
+  const calculate = (priceUSD) => {
+    setResult(priceUSD * selCoins);
+  }
+
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      })
+  }, []); // 빈 배열이면 한번만 동작
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading
+        ? <strong>Loading....</strong>
+        : <select onChange={onChange}>
+          {coins.map((coin, index) =>
+            <option
+              key={index}
+              value={coin.quotes.USD.price}
+            >{coin.name} : {coin.quotes.USD.price}
+            </option>
+          )}
+        </select>}
+      <form onSubmit={onSubmit}>
+        <input type="number" placeholder="write us dollars" />
+        <button>change coins</button>
+      </form>
+      <h2>you can change {result} coins!</h2>
     </div>
   );
 }
